7f1eb54c97fa463a894719939cde0c5a3aa754a8
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-03-23 12:48:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -40,5 +40,5 @@ var r = new XMLHttpRequest();
 r.addEventListener("load", function () {
     document.querySelector("#stats").innerText = "Unique Visits: " + JSON.parse(this.responseText).count_unique + "\nTotal Visits: " + JSON.parse(this.responseText).count;
 });
-r.open("GET", "https://frcat.goatcounter.com/counter/" + encodeURIComponent(location.pathname) + ".json");
+r.open("GET", "https://gc.frcat.workers.dev/json");
 r.send();
