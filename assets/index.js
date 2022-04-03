12e379d3933543d13c07242eec00683cb9ac4fd3
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-04-03 16:16:38 -0400

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,20 +1,19 @@
-// fetch status
+// Status
 function status() {
     fetch("https://canary.discord.com/api/guilds/957380894186946623/widget.json")
         .then((response) => response.json())
-        .then((data) => user(data.members[0].status));
-}
-status();
-// work with status
-function user(status) {
-    if (status == "online") {
+        .then((data) => {
+        const status = data.members[0].status;
+            if (status == "online") {
         document.getElementById("pfp").style = "border: 5px solid #3BA55D";
     } else if (status == "idle") {
         document.getElementById("pfp").style = "border: 5px solid #FAA81A";
     } else if (status == "dnd") {
         document.getElementById("pfp").style = "border: 5px solid #ED4245";
     }
+    });
 }
+status();
 // Context menu
 const myContextMenu = new window.VanillaContextMenu({
     scope: document.body,
@@ -27,3 +26,9 @@ const myContextMenu = new window.VanillaContextMenu({
     ],
     preventCloseOnClick: true,
 });
+// Titles
+    fetch("https://api.statify.live/youtube/UCh--dzF5q_VM5HCrRJN1t2w")
+        .then((response) => response.json())
+        .then((data) => {
+    //
+    });
