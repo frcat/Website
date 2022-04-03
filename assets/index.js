1aef2de993515cf6e8993ebe40264f2daefdca34
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-04-03 15:26:06 -0400

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,20 +1,19 @@
 // fetch status
 function status() {
-fetch("https://canary.discord.com/api/guilds/957380894186946623/widget.json")
-  .then(response => response.json())
-  .then(data => user(data.members[0].status));
+    fetch("https://canary.discord.com/api/guilds/957380894186946623/widget.json")
+        .then((response) => response.json())
+        .then((data) => user(data.members[0].status));
 }
-status()
+status();
 // work with status
 function user(status) {
-        if (status == "online") {
-            document.getElementById("pfp").style = "border: 5px solid #3BA55D";
-        } else if (status == "idle") {
-            document.getElementById("pfp").style = "border: 5px solid #FAA81A";
-        } else if ((status == "dnd")) {
-            document.getElementById("pfp").style = "border: 5px solid #ED4245";
-        }
-
+    if (status == "online") {
+        document.getElementById("pfp").style = "border: 5px solid #3BA55D";
+    } else if (status == "idle") {
+        document.getElementById("pfp").style = "border: 5px solid #FAA81A";
+    } else if (status == "dnd") {
+        document.getElementById("pfp").style = "border: 5px solid #ED4245";
+    }
 }
 // Visits
 var r = new XMLHttpRequest();
@@ -24,14 +23,14 @@ r.addEventListener("load", function () {
 r.open("GET", "https://frcat.goatcounter.com/counter//.json");
 r.send();
 // Context menu
-      const myContextMenu = new window.VanillaContextMenu({
-        scope: document.body,
-        menuItems: [
-          {
-            label: 'Refresh Status',
+const myContextMenu = new window.VanillaContextMenu({
+    scope: document.body,
+    menuItems: [
+        {
+            label: "Refresh Status",
             callback: status,
             preventCloseOnClick: false,
-          },
-        ],
-        preventCloseOnClick: true,
-      });
+        },
+    ],
+    preventCloseOnClick: true,
+});
