f0c7c38ea1b7ac3bf1d3c16fe06dcf04eef9bb0d
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-04-04 01:19:21 -0400

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -3,15 +3,15 @@ function status() {
     fetch("https://canary.discord.com/api/guilds/957380894186946623/widget.json")
         .then((response) => response.json())
         .then((data) => {
-        const status = data.members[0].status;
+            const status = data.members[0].status;
             if (status == "online") {
-        document.getElementById("pfp").style = "border: 5px solid #3BA55D";
-    } else if (status == "idle") {
-        document.getElementById("pfp").style = "border: 5px solid #FAA81A";
-    } else if (status == "dnd") {
-        document.getElementById("pfp").style = "border: 5px solid #ED4245";
-    }
-    });
+                document.getElementById("pfp").style = "border: 5px solid #3BA55D";
+            } else if (status == "idle") {
+                document.getElementById("pfp").style = "border: 5px solid #FAA81A";
+            } else if (status == "dnd") {
+                document.getElementById("pfp").style = "border: 5px solid #ED4245";
+            }
+        });
 }
 status();
 // Context menu
@@ -27,23 +27,23 @@ const myContextMenu = new window.VanillaContextMenu({
     preventCloseOnClick: true,
 });
 // Titles
-    fetch("https://api.statify.live/youtube/UCh--dzF5q_VM5HCrRJN1t2w")
-        .then((response) => response.json())
-        .then((data) => {
-    document.getElementById("youtube").title = data
+fetch("https://api.statify.live/youtube/UCh--dzF5q_VM5HCrRJN1t2w")
+    .then((response) => response.json())
+    .then((data) => {
+        document.getElementById("youtube").title = data;
     });
-    fetch("https://api.statify.live/discord/Grauj95zDw")
-        .then((response) => response.json())
-        .then((data) => {
-    document.getElementById("discord").title = data
+fetch("https://api.statify.live/discord/Grauj95zDw")
+    .then((response) => response.json())
+    .then((data) => {
+        document.getElementById("discord").title = data;
     });
-    fetch("https://api.statify.live/twitch/aFrenchCat")
-        .then((response) => response.json())
-        .then((data) => {
-    document.getElementById("twitch").title = data
+fetch("https://api.statify.live/twitch/aFrenchCat")
+    .then((response) => response.json())
+    .then((data) => {
+        document.getElementById("twitch").title = data;
     });
-    fetch("https://api.statify.live/twitter/Fr3nch_C4t")
-        .then((response) => response.json())
-        .then((data) => {
-    document.getElementById("twitter").title = data
+fetch("https://api.statify.live/twitter/Fr3nch_C4t")
+    .then((response) => response.json())
+    .then((data) => {
+        document.getElementById("twitter").title = data;
     });
