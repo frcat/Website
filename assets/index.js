951a2a368a3423d8e555a6ae3662c8df4f7e969c
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-04-10 18:48:22 -0400

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -30,8 +30,7 @@ window.onload = () => {
                 } else {
                     UserObject.avatar = `https://cdn.discordapp.com/avatars/${userData.id}/${userData.avatar}.png`;
                 }
-                document.getElementById('userName').innerText =
-                    UserObject.username;
+                Typing(UserObject.username);
                 document.getElementById('userAvatar').src = UserObject.avatar;
                 console.log(UserObject.status);
                 if (UserObject.status == 'online') {
