ab38fbf36260c286dd0c40fe6a6ebb3b5858ba32
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-05-08 15:14:47 -0400

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,70 +1,20 @@
-'use strict';
-const whitelistedUsers = [
-    '811770910624579584',
-];
-
-window.onload = () => {
-    function loadStatus() {
-        const lanyardSocket = new WebSocket('wss://api.aspy.dev/frcat/lanyard');
-        lanyardSocket.onopen = () => {
-            lanyardSocket.send(
-                JSON.stringify({
-                    op: 2,
-                    d: { subscribe_to_ids: whitelistedUsers },
-                }),
-            );
-        };
-        lanyardSocket.onmessage = async (message) => {
-            const { d } = await JSON.parse(message.data);
-            for (const rawUserData of Object.values(d)) {
-                const userData = rawUserData?.discord_user;
-                if (!userData) continue;
-                const UserObject = {
-                    username: `${userData.username}#${userData.discriminator}`,
-                    status: rawUserData.discord_status,
-                };
-                lanyardSocket.close();
-                if (userData.avatar?.startsWith('a_')) {
-                    UserObject.avatar = `https://cdn.discordapp.com/avatars/${userData.id}/${userData.avatar}.gif`;
-                } else {
-                    UserObject.avatar = `https://cdn.discordapp.com/avatars/${userData.id}/${userData.avatar}.png`;
-                }
-                document.getElementById("text").innerText = UserObject.username
-                document.getElementById('userAvatar').src = UserObject.avatar;
-                console.log(UserObject.status);
-                if (UserObject.status == 'online') {
+// Quote
+fetch("https://quotes.French-Cat.repl.co/raw").then(res => res.text()).then(data => document.getElementById("quote").innerText = data)
+// Discord
+fetch("https://discord.com/api/guilds/972931047069200384/widget.json").then(res => res.text()).then(data => dc(data))
+function dc(data) {
+    if (!data.members==""){
+                    if (data.members[0].status == 'online') {
                     document.getElementById('userAvatar').style =
                         'border: 5px solid #3BA55D';
-                } else if (UserObject.status == 'idle') {
+                } else if (data.members[0].status == 'idle') {
                     document.getElementById('userAvatar').style =
                         'border: 5px solid #FAA81A';
-                } else if (UserObject.status == 'dnd') {
+                } else if (data.members[0].status == 'dnd') {
                     document.getElementById('userAvatar').style =
                         'border: 5px solid #ED4245';
                 }
-            }
-        };
     }
-    const myContextMenu = new window.VanillaContextMenu({
-        scope: document.body,
-        customThemeClass: 'contextMenuTheme',
-        menuItems: [
-            {
-                label: 'Refresh Status',
-                callback: refresh,
-            },
-        ],
-        preventCloseOnClick: false,
-    });
-    // Refresh Func
-    function refresh() {
-        loadStatus();
-    }
-    // Init
-    refresh();
-    // Theme
-    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
-    if (darkThemeMq.matches) { } else {
-        document.getElementById("theme").href = "assets/index-light.css";
-    }
-};
+}
+// Theme
+if (!window.matchMedia("(prefers-color-scheme: dark)")){document.getElementById("theme").href = "assets/index-light.css";}
