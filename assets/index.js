52353d9e91d85bedac8a4bd769c5d6ea55fbf156
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-05-26 12:57:16 -0400

    make discord pfp less pixely

--- assets/index.js
+++ assets/index.js
@@ -5,7 +5,7 @@ document.addEventListener('mouseover',(data=>{document.getElementById("quik").hr
 // Discord
 fetch(["https://discord.com/api/guilds/972931047069200384/widget.json?","https://ptb.discord.com/api/guilds/972931047069200384/widget.json?","https://canary.discord.com/api/guilds/972931047069200384/widget.json?"][Math.floor(Math.random()*["https://discord.com/api/guilds/972931047069200384/widget.json?","https://ptb.discord.com/api/guilds/972931047069200384/widget.json?","https://canary.discord.com/api/guilds/972931047069200384/widget.json?"].length)] + new Date().valueOf()).then(res => res.json()).then(data => {
     if (!data.members.length == 0) {
-        document.getElementById('userAvatar').src = data.members[0].avatar_url;
+        document.getElementById('userAvatar').src = data.members[0].avatar_url+"?size=1024";
         if (data.members[0].status == 'online') {
             document.getElementById('userAvatar').style =
                 'border: 5px solid #3BA55D';
