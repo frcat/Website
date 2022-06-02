760659777bec9d83fa75f14664ae0e9e2e273c44
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-06-01 23:01:05 -0400

    Update 404.js

--- blog/assets/404.js
+++ blog/assets/404.js
@@ -1,6 +1,6 @@
 fetch(window.location.href.replace("https://frcat.win/blog/", "https://frcat.win/blog/posts/") + ".md")
     .then((resp) => resp.text())
     .then((data) => {
-        document.body.innerHTML = '<center><a href="." style="font-size:150%">🔙</a>' + new showdown.Converter().makeHtml(data) + "</center>";
+        document.body.innerHTML = '<center><a href="https://frcat.win/blog" style="font-size:150%">🔙</a>' + new showdown.Converter().makeHtml(data) + "</center>";
         twemoji.parse(document);
     });
