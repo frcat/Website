9922f97268909879468831ffb0198cb48935608e
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-06-01 22:52:04 -0400

    Create index.js

---
+++ blog/assets/index.js
@@ -0,0 +1,19 @@
+fetch("https://frcat.win/blog/posts/index.json")
+    .then((response) => response.json())
+    .then((data) => {
+        data.forEach(build);
+    });
+
+function build(data) {
+    document.getElementById("cont").innerHTML += document.getElementById("post").innerHTML;
+    const title = document.getElementById("title");
+    const desc = document.getElementById("desc");
+    const link = document.getElementById("link");
+
+    title.innerText = data.Title;
+    desc.innerText = data.Desc;
+    link.href = data.id;
+    title.id = data.id;
+    desc.id = data.id;
+    link.id = data.id;
+}
