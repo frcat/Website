5defe3300e3233116973bcfe6f4987c142ef7572
French-Cat <src@frcat.win>
2022-06-06 14:54:16 -0400

    Update

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,22 @@
+const widget = ["https://discord.com/api/guilds/972931047069200384/widget.json?", "https://ptb.discord.com/api/guilds/972931047069200384/widget.json?", "https://canary.discord.com/api/guilds/972931047069200384/widget.json?"]
+// Links
+fetch("links.min.json")
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(ldata => {
+            document.getElemenetById("links").innerHTML += document.getElementById("link").innerHTML
+            document.getElementById("href").href = ldata.href
+            document.getElementById("icon").classList.add(ldata.icon)
+            document.getElementById("href").id = ldata.icon
+            document.getElementById("icon").id = ldata.icon
+        }
+        )
+    }
+    )
 // Discord
-fetch(["https://discord.com/api/guilds/972931047069200384/widget.json?","https://ptb.discord.com/api/guilds/972931047069200384/widget.json?","https://canary.discord.com/api/guilds/972931047069200384/widget.json?"][Math.floor(Math.random()*["https://discord.com/api/guilds/972931047069200384/widget.json?","https://ptb.discord.com/api/guilds/972931047069200384/widget.json?","https://canary.discord.com/api/guilds/972931047069200384/widget.json?"].length)] + new Date().valueOf()).then(res => res.json()).then(data => {
+fetch(widget[Math.floor(Math.random() * widget.length)] + new Date().valueOf()).then(res => res.json()).then(data => {
     if (!data.members.length == 0) {
-        document.getElementById('userAvatar').src = data.members[0].avatar_url+"?size=1024";
+        document.getElementById('userAvatar').src = data.members[0].avatar_url + "?size=1024";
         if (data.members[0].status == 'online') {
             document.getElementById('userAvatar').style =
                 'border: 5px solid #3BA55D';
@@ -20,8 +35,8 @@ fetch(["https://discord.com/api/guilds/972931047069200384/widget.json?","https:/
 fetch("https://quotes.French-Cat.repl.co/raw").then(res => res.text()).then(quote => document.getElementById("quote").innerText = quote)
 // Hashtag
 function color() {
-    var item = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'][Math.floor(Math.random()*['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'].length)];
+    var item = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'][Math.floor(Math.random() * ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'].length)];
     document.getElementById("hashtag").style = `color:${item};text-shadow:2px 2px 2px ${item};`
-  }
+}
 color()
-setInterval(color,1050)
+setInterval(color, 1050)
