93a0da10908296dc8433347dc14fb10ee002ff2f
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-06-19 16:35:38 -0400

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,34 +1,33 @@
 // Links
 fetch("assets/links.json")
-    .then(res => res.json())
-    .then(data => {
-        data.forEach(link => {
-            document.getElementById("links").innerHTML += document.getElementById("link").innerHTML
-            document.getElementById("href").href = link.href
-            document.getElementById("icon").classList.add(link.icon)
-            document.getElementById("href").id = link.icon
-            document.getElementById("icon").id = link.icon
-        }
-        )
-    }
-    )
+    .then((res) => res.json())
+    .then((data) => {
+        data.forEach((link) => {
+            document.getElementById("links").innerHTML += document.getElementById("link").innerHTML;
+            document.getElementById("href").href = link.href;
+            document.getElementById("icon").classList.add(link.icon);
+            document.getElementById("href").id = link.icon;
+            document.getElementById("icon").id = link.icon;
+        });
+    });
 // Discord
-fetch("https://canary.discord.com/api/guilds/966332825492160542/widget.json" + new Date().valueOf()).then(res => res.json()).then(data => {
-    if (!data.members.length == 0) {
-        document.getElementById('userAvatar').src = data.members[0].avatar_url + "?size=1024";
-        if (data.members[0].status == 'online') {
-            document.getElementById('userAvatar').style =
-                'border: 5px solid #3BA55D';
-        } else if (data.members[0].status == 'idle') {
-            document.getElementById('userAvatar').style =
-                'border: 5px solid #FAA81A';
-        } else if (data.members[0].status == 'dnd') {
-            document.getElementById('userAvatar').style =
-                'border: 5px solid #ED4245';
+fetch("https://canary.discord.com/api/guilds/966332825492160542/widget.json" + new Date().valueOf())
+    .then((res) => res.json())
+    .then((data) => {
+        if (!data.members.length == 0) {
+            document.getElementById("userAvatar").src = data.members[0].avatar_url + "?size=1024";
+            if (data.members[0].status == "online") {
+                document.getElementById("userAvatar").style = "border: 5px solid #3BA55D";
+            } else if (data.members[0].status == "idle") {
+                document.getElementById("userAvatar").style = "border: 5px solid #FAA81A";
+            } else if (data.members[0].status == "dnd") {
+                document.getElementById("userAvatar").style = "border: 5px solid #ED4245";
+            }
+        } else {
+            document.getElementById("userAvatar").src = "https://github.com/French-Cat.png";
         }
-    } else {
-        document.getElementById('userAvatar').src = "https://github.com/French-Cat.png";
-    }
-})
+    });
 // Quote
-fetch("https://quotes.French-Cat.repl.co/raw").then(res => res.text()).then(quote => document.getElementById("quote").innerText = quote)
+fetch("https://quotes.French-Cat.repl.co/raw")
+    .then((res) => res.text())
+    .then((quote) => (document.getElementById("quote").innerText = quote));
